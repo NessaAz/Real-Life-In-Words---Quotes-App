57edbb025fe1952ef9fcb0073c7b4714bfd8bd28
NessaAz <vanessa.azenwa@gmail.com>
2022-04-14 04:36:22 +0300

    put button to upvote

--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -8,7 +8,7 @@ import { Quotes } from '../quote';
 })
 export class QuotesComponent implements OnInit {
   quotes: Quotes [] = [
-    new Quotes(1, 'Shine like the whole universe is yours!', 0, 0),
+    new Quotes(1, 'Shine like the whole universe is yours! - Anon', 0, 0),
     new Quotes(2, 'Some of your best days have not even happened yet - Anne Frank', 0, 0),
     new Quotes(3, 'The Sun will rise and we will try again - 21  PILOTS', 0, 0),
   ] 
