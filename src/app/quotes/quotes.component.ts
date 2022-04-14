c9f010b68b10ac28dbfcd7f4b9df8f8dcb90ce40
NessaAz <vanessa.azenwa@gmail.com>
2022-04-14 04:25:59 +0300

    add the user input form

--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -10,7 +10,7 @@ export class QuotesComponent implements OnInit {
   quotes: Quotes [] = [
     new Quotes(1, 'Shine like the whole universe is yours!', 0, 0),
     new Quotes(2, 'Some of your best days have not even happened yet - Anne Frank', 0, 0),
-    new Quotes(3, 'The Sun will rise and we will try again - 2 PILOTS', 0, 0),
+    new Quotes(3, 'The Sun will rise and we will try again - 21  PILOTS', 0, 0),
   ] 
   constructor() { }
 
